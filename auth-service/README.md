6e0858fd1bddf6e3ccc08fa79ed7433dc28e48af
Kutman Smanov <smanovkutman0258@gmail.com>
2025-06-05 15:49:45 +0600

    initial point

---
+++ auth-service/README.md
@@ -0,0 +1,8 @@
+# Auth Service
+
+Микросервис аутентификации и авторизации для маркетплейса "Бирге Базар".
+
+- Spring Cloud Config Client (bootstrap.yml)
+- Eureka Discovery Client
+- Circuit Breaker (Resilience4j)
+- Пример настроек берется из Config Server
---
+++ tracking-service/src/main/java/kg/birge/bazar/trackingservice/TrackingServiceApplication.java
@@ -0,0 +1,11 @@
+package kg.birge.bazar.trackingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackingServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(TrackingServiceApplication.class, args);
+    }
+}
